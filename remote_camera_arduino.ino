6ed8488e1f3f7f8d0369e1377d7a741d66ffc7af
Corentin LEMAITRE <corentinlemaitre@gmail.com>
2021-06-08 19:40:40 +0200

    Ajout de passage de Auto_mode à mode vélo

--- remote_camera_arduino.ino
+++ remote_camera_arduino.ino
@@ -8,9 +8,10 @@ int led = 13;
 int capture = 1;
 unsigned long action_time = 3000;
 bool auto_mode = true;
-
+int ntour = 0;
 int capteur = 9;     // compteur de tour
 int tmax = 7;    // 2 aimants pour une roue de 700 : 0,5 tour = 1,065m / 1 aimant pour une roue de brompton 1 tour = 1,33
+unsigned long button_timer ;
 
 
 void setup() {
@@ -38,11 +39,15 @@ void loop() {
 
     if (digitalRead(bouton) == LOW) {
       digitalWrite(led, HIGH);
+      button_timer = millis();
       while (digitalRead(bouton) == LOW) {
         delay(10);
       }
       digitalWrite(led, LOW);
-      if (capture == 1) {
+      if (millis()-button_timer > 2000L) {
+        auto_mode = false;
+      }
+      else if (capture == 1) {
         capture = 0;
       }
       else {
@@ -54,26 +59,31 @@ void loop() {
   }
   else
   {
-    int ntour = 0;
-    while (ntour < tmax) {
-      if (digitalRead(capteur) == LOW) {
-        digitalWrite(led, HIGH);
-        ntour++;
-        while (digitalRead(capteur) == LOW) {
-          delay(10);
-        }
-        digitalWrite(led, LOW);
-      }
-      if (digitalRead(bouton) == LOW) {
-        digitalWrite(led, HIGH);
-        ntour = tmax;
-        while (digitalRead(bouton) == LOW) {
-          delay(10);
-        }
-        digitalWrite(led, LOW);
-      }
+    if (ntour >= tmax) {
+      prendre_photo();
+      ntour = 0;
     }
-    prendre_photo();
+    if (digitalRead(capteur) == LOW) {
+      digitalWrite(led, HIGH);
+      ntour++;
+      while (digitalRead(capteur) == LOW) {
+        delay(10);
+      }
+      digitalWrite(led, LOW);
+    }
+    if (digitalRead(bouton) == LOW) {
+      digitalWrite(led, HIGH);
+      button_timer = millis();
+      while (digitalRead(bouton) == LOW) {
+        delay(10);
+      }
+      if (millis()-button_timer > 2000L) {
+        auto_mode = true;
+        capture = 1;      // on active le mode prise de vue automatique
+      }
+      ntour = tmax;
+      digitalWrite(led, LOW);
+    }
   }
 
 }
