b3d4e2fbf4b10d2d19cdb09310c32f49aafa0599
Corentin LEMAITRE <corentinlemaitre@gmail.com>
2021-06-08 18:34:52 +0200

    Nettoyage de code arduino et variable

--- remote_camera_arduino.ino
+++ remote_camera_arduino.ino
@@ -1,52 +1,53 @@
-// Version 4 du 27/01/2020
-
 // BUT : prise de photo toutes les secondes + possibilité de pause avec intervale d'une seconde qui continue à etre suivi
 // Capteur de tour + Bouton photo + Prise de photo (D9 D10 D11)
 // cable blanc un sur le GND, l'autre sur le 9
-// cable 
-int bouton = {10};   // bouton de prise de photo
-int shoot = {11};   // actionneur de photo
-int tourled = {13}; // la led qui sera activé quand on fait un tour 
+// cable
+int bouton = 10;   // bouton de prise de photo
+int action_pin = 11;   // actionneur de photo
+int led = 13; // la led qui sera activé quand on fait un tour
 int capture = 1;
-unsigned long time_saved = 3000;
-unsigned long time;
+unsigned long action_time = 3000;
 
 
 void setup() {
-    pinMode(bouton, INPUT_PULLUP); 
-    pinMode(shoot, OUTPUT);
-    pinMode(tourled, OUTPUT);
-    digitalWrite(shoot, LOW);
+  pinMode(bouton, INPUT_PULLUP);
+  pinMode(action_pin, OUTPUT);
+  pinMode(led, OUTPUT);
+  digitalWrite(action_pin, LOW);
 }
 
 
-
-// the loop function runs over and over again forever
 void loop() {
-  time = millis();
-  if (time > time_saved) {
+
+  if ( millis() >= action_time) {
     if (capture == 1) {
-      digitalWrite(shoot, HIGH); //le but est de shunter un interupteur
-      digitalWrite(tourled, HIGH);
-      delay(100);
-      digitalWrite(shoot, LOW); // reste appuyer jusqu'a la prochaine photo
-      digitalWrite(tourled, LOW); 
+      prendre_photo();
     }
-    time_saved = time_saved + 1000;
+    action_time = action_time + 1000L;
   }
-  
+
   if (digitalRead(bouton) == LOW) {
-    digitalWrite(tourled, HIGH);
-    while (digitalRead(bouton) == LOW){
+    digitalWrite(led, HIGH);
+    while (digitalRead(bouton) == LOW) {
       delay(10);
     }
-    digitalWrite(tourled, LOW);
+    digitalWrite(led, LOW);
     if (capture == 1) {
       capture = 0;
     }
     else {
       capture = 1;
+      action_time = millis();
     }
   }
-  delay(10);   
+  delay(10);
+}
+
+void prendre_photo()
+{
+  digitalWrite(action_pin, HIGH); // shunt interupteur de prise de photo via optocoupleur
+  digitalWrite(led, HIGH);
+  delay(100);                     // délai pour prise en compte de l'appui prise de photo
+  digitalWrite(action_pin, LOW);
+  digitalWrite(led, LOW);
 }
